dfcc6a477086ae5002f37985c15941a2c86bd6ce
Kenneth Tuten <ktuten@fb.com>
2022-01-25 21:04:32 -0600

    fixed spacing issues

--- dehy/settings.py
+++ dehy/settings.py
@@ -18,8 +18,8 @@ from pathlib import Path
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 env = environ.Env(
-    # set casting, default value
-    DEBUG=(bool, False)
+	# set casting, default value
+	DEBUG=(bool, False)
 )
 environ.Env.read_env()
 
@@ -41,89 +41,89 @@ ALLOWED_HOSTS = env('ALLOWED_HOSTS')
 # Application definition
 
 INSTALLED_APPS = [
-    'django.contrib.admin',
-    'django.contrib.auth',
-    'django.contrib.contenttypes',
-    'django.contrib.sessions',
-    'django.contrib.messages',
-    'django.contrib.staticfiles',
+	'django.contrib.admin',
+	'django.contrib.auth',
+	'django.contrib.contenttypes',
+	'django.contrib.sessions',
+	'django.contrib.messages',
+	'django.contrib.staticfiles',
 	# django apps added by oscar
 	'django.contrib.sites',
-    'django.contrib.flatpages',
+	'django.contrib.flatpages',
 	# oscar apps
-    'oscar.config.Shop',
-    'oscar.apps.analytics.apps.AnalyticsConfig',
-    'oscar.apps.checkout.apps.CheckoutConfig',
-    'oscar.apps.address.apps.AddressConfig',
-    'oscar.apps.shipping.apps.ShippingConfig',
-    'oscar.apps.catalogue.apps.CatalogueConfig',
-    'oscar.apps.catalogue.reviews.apps.CatalogueReviewsConfig',
-    'oscar.apps.communication.apps.CommunicationConfig',
-    'oscar.apps.partner.apps.PartnerConfig',
-    'oscar.apps.basket.apps.BasketConfig',
-    'oscar.apps.payment.apps.PaymentConfig',
-    'oscar.apps.offer.apps.OfferConfig',
-    'oscar.apps.order.apps.OrderConfig',
-    'oscar.apps.customer.apps.CustomerConfig',
-    'oscar.apps.search.apps.SearchConfig',
-    'oscar.apps.voucher.apps.VoucherConfig',
-    'oscar.apps.wishlists.apps.WishlistsConfig',
-    'oscar.apps.dashboard.apps.DashboardConfig',
-    'oscar.apps.dashboard.reports.apps.ReportsDashboardConfig',
-    'oscar.apps.dashboard.users.apps.UsersDashboardConfig',
-    'oscar.apps.dashboard.orders.apps.OrdersDashboardConfig',
-    'oscar.apps.dashboard.catalogue.apps.CatalogueDashboardConfig',
-    'oscar.apps.dashboard.offers.apps.OffersDashboardConfig',
-    'oscar.apps.dashboard.partners.apps.PartnersDashboardConfig',
-    'oscar.apps.dashboard.pages.apps.PagesDashboardConfig',
-    'oscar.apps.dashboard.ranges.apps.RangesDashboardConfig',
-    'oscar.apps.dashboard.reviews.apps.ReviewsDashboardConfig',
-    'oscar.apps.dashboard.vouchers.apps.VouchersDashboardConfig',
-    'oscar.apps.dashboard.communications.apps.CommunicationsDashboardConfig',
-    'oscar.apps.dashboard.shipping.apps.ShippingDashboardConfig',
+	'oscar.config.Shop',
+	'oscar.apps.analytics.apps.AnalyticsConfig',
+	'oscar.apps.checkout.apps.CheckoutConfig',
+	'oscar.apps.address.apps.AddressConfig',
+	'oscar.apps.shipping.apps.ShippingConfig',
+	'oscar.apps.catalogue.apps.CatalogueConfig',
+	'oscar.apps.catalogue.reviews.apps.CatalogueReviewsConfig',
+	'oscar.apps.communication.apps.CommunicationConfig',
+	'oscar.apps.partner.apps.PartnerConfig',
+	'oscar.apps.basket.apps.BasketConfig',
+	'oscar.apps.payment.apps.PaymentConfig',
+	'oscar.apps.offer.apps.OfferConfig',
+	'oscar.apps.order.apps.OrderConfig',
+	'oscar.apps.customer.apps.CustomerConfig',
+	'oscar.apps.search.apps.SearchConfig',
+	'oscar.apps.voucher.apps.VoucherConfig',
+	'oscar.apps.wishlists.apps.WishlistsConfig',
+	'oscar.apps.dashboard.apps.DashboardConfig',
+	'oscar.apps.dashboard.reports.apps.ReportsDashboardConfig',
+	'oscar.apps.dashboard.users.apps.UsersDashboardConfig',
+	'oscar.apps.dashboard.orders.apps.OrdersDashboardConfig',
+	'oscar.apps.dashboard.catalogue.apps.CatalogueDashboardConfig',
+	'oscar.apps.dashboard.offers.apps.OffersDashboardConfig',
+	'oscar.apps.dashboard.partners.apps.PartnersDashboardConfig',
+	'oscar.apps.dashboard.pages.apps.PagesDashboardConfig',
+	'oscar.apps.dashboard.ranges.apps.RangesDashboardConfig',
+	'oscar.apps.dashboard.reviews.apps.ReviewsDashboardConfig',
+	'oscar.apps.dashboard.vouchers.apps.VouchersDashboardConfig',
+	'oscar.apps.dashboard.communications.apps.CommunicationsDashboardConfig',
+	'oscar.apps.dashboard.shipping.apps.ShippingDashboardConfig',
 
-    # 3rd-party apps that oscar depends on
-    'widget_tweaks',
-    'haystack',
-    'treebeard',
-    'sorl.thumbnail',   # Default thumbnail backend, can be replaced
-    'django_tables2',
+	# 3rd-party apps that oscar depends on
+	'widget_tweaks',
+	'haystack',
+	'treebeard',
+	'sorl.thumbnail',   # Default thumbnail backend, can be replaced
+	'django_tables2',
 ]
 
 SITE_ID = 1
 
 MIDDLEWARE = [
-    'django.middleware.security.SecurityMiddleware',
-    'django.contrib.sessions.middleware.SessionMiddleware',
-    'django.middleware.common.CommonMiddleware',
-    'django.middleware.csrf.CsrfViewMiddleware',
-    'django.contrib.auth.middleware.AuthenticationMiddleware',
-    'django.contrib.messages.middleware.MessageMiddleware',
-    'django.middleware.clickjacking.XFrameOptionsMiddleware',
+	'django.middleware.security.SecurityMiddleware',
+	'django.contrib.sessions.middleware.SessionMiddleware',
+	'django.middleware.common.CommonMiddleware',
+	'django.middleware.csrf.CsrfViewMiddleware',
+	'django.contrib.auth.middleware.AuthenticationMiddleware',
+	'django.contrib.messages.middleware.MessageMiddleware',
+	'django.middleware.clickjacking.XFrameOptionsMiddleware',
 	'oscar.apps.basket.middleware.BasketMiddleware',
-    'django.contrib.flatpages.middleware.FlatpageFallbackMiddleware',
+	'django.contrib.flatpages.middleware.FlatpageFallbackMiddleware',
 ]
 
 ROOT_URLCONF = 'dehy.urls'
 
 TEMPLATES = [
-    {
-        'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [BASE_DIR / 'templates'],
-        'APP_DIRS': True,
-        'OPTIONS': {
-            'context_processors': [
-                'django.template.context_processors.debug',
-                'django.template.context_processors.request',
-                'django.contrib.auth.context_processors.auth',
-                'django.contrib.messages.context_processors.messages',
+	{
+		'BACKEND': 'django.template.backends.django.DjangoTemplates',
+		'DIRS': [BASE_DIR / 'templates'],
+		'APP_DIRS': True,
+		'OPTIONS': {
+			'context_processors': [
+				'django.template.context_processors.debug',
+				'django.template.context_processors.request',
+				'django.contrib.auth.context_processors.auth',
+				'django.contrib.messages.context_processors.messages',
 				'oscar.apps.search.context_processors.search_form',
 				'oscar.apps.checkout.context_processors.checkout',
 				'oscar.apps.communication.notifications.context_processors.notifications',
 				'oscar.core.context_processors.metadata',
-            ],
-        },
-    },
+			],
+		},
+	},
 ]
 
 WSGI_APPLICATION = 'dehy.wsgi.application'
@@ -133,18 +133,18 @@ WSGI_APPLICATION = 'dehy.wsgi.application'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        #'ENGINE': 'django.db.backends.sqlite3',
-        #'NAME': BASE_DIR / 'db.sqlite3',
+	'default': {
+		#'ENGINE': 'django.db.backends.sqlite3',
+		#'NAME': BASE_DIR / 'db.sqlite3',
 
-        'ENGINE': 'django.db.backends.postgresql',
-        'HOST': 'localhost',
-        'NAME': 'DEHY',
-        'PASSWORD': 'penileZZ44yN0tT420',
-        'PORT': '5432',
-        'USER': 'dehydevuser',
-        'ATOMIC_REQUESTS': True,
-    }
+		'ENGINE': 'django.db.backends.postgresql',
+		'HOST': 'localhost',
+		'NAME': 'DEHY',
+		'PASSWORD': 'penileZZ44yN0tT420',
+		'PORT': '5432',
+		'USER': 'dehydevuser',
+		'ATOMIC_REQUESTS': True,
+	}
 }
 
 
@@ -169,18 +169,18 @@ DATABASES = {
 # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
 
 AUTH_PASSWORD_VALIDATORS = [
-    {
-        'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
-    },
-    {
-        'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator',
-    },
-    {
-        'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator',
-    },
-    {
-        'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
-    },
+	{
+		'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
+	},
+	{
+		'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator',
+	},
+	{
+		'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator',
+	},
+	{
+		'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
+	},
 ]
 
 
@@ -215,12 +215,12 @@ MEDIA_ROOT = BASE_DIR / "media"
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 AUTHENTICATION_BACKENDS = [
-    'oscar.apps.customer.auth_backends.EmailBackend',
-    'django.contrib.auth.backends.ModelBackend',
+	'oscar.apps.customer.auth_backends.EmailBackend',
+	'django.contrib.auth.backends.ModelBackend',
 ]
 
 HAYSTACK_CONNECTIONS = {
-    'default': {
-        'ENGINE': 'haystack.backends.simple_backend.SimpleEngine',
-    },
+	'default': {
+		'ENGINE': 'haystack.backends.simple_backend.SimpleEngine',
+	},
 }
