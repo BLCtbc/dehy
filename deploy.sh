1fed0789012a2f6f15b168f9c8b8a9c695a8092a
Kenneth Tuten <ktuten@fb.com>
2022-02-11 15:45:19 -0600

    gogogo

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,6 @@
-#!/bin/bash
+#!/bin/sh
 
-source /venv/bin/activate
+source venv/bin/activate
 pip install -r requirements.txt
 python manage.py collectstatic --noinput && python manage.py makemigrations && python manage.py migrate
 sudo systemctl daemon-reload
