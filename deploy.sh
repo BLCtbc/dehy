c1e7073cc702e4fb5209c895c4e150e42fa6a1b7
Kenneth Tuten <ktuten@fb.com>
2022-02-11 15:27:35 -0600

    gogogo

--- deploy.sh
+++ deploy.sh
@@ -5,3 +5,4 @@ pip install -r requirements.txt
 python manage.py collectstatic --noinput && python manage.py makemigrations && python manage.py migrate
 sudo systemctl daemon-reload
 sudo systemctl restart nginx && sudo systemctl restart gunicorn
+deactivate
