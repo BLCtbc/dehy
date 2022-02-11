e435912716367aee1bee27b0cf214a158c029cbb
Kenneth Tuten <ktuten@fb.com>
2022-02-11 13:33:20 -0600

    aa

--- deploy.sh
+++ deploy.sh
@@ -1,11 +1,11 @@
 #!/bin/sh
 cd ~/home/admin/dehy/dehy
-git checkout staging
 git pull
-python manage.py makemigrations
-python manage.py migrate
+python3.7 -m venv venv
 source venv/bin/activate
 pip install -r requirements.txt
 echo 'hello world'
 python manage.py collectstatic --noinput
+python manage.py makemigrations
+python manage.py migrate
 sudo systemctl restart nginx && sudo systemctl restart gunicorn
