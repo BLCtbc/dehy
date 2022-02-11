b706293cf5b2761334906a5223d026b79af41513
Kenneth Tuten <ktuten@fb.com>
2022-02-11 15:24:55 -0600

    gogogo

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
 
-source "/home/admin/dehy/dehy/.venv/bin/activate" && pip install -r requirements.txt && python manage.py collectstatic --noinput && python manage.py makemigrations && python manage.py migrate
+source /home/admin/dehy/dehy/venv/bin/activate
+pip install -r requirements.txt
+python manage.py collectstatic --noinput && python manage.py makemigrations && python manage.py migrate
 sudo systemctl daemon-reload
 sudo systemctl restart nginx && sudo systemctl restart gunicorn
