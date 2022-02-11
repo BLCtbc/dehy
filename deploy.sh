e5d1c110f46b44a188b8af80aeb87a4cc3495515
Kenneth Tuten <ktuten@fb.com>
2022-02-11 13:54:16 -0600

    asdfa

--- deploy.sh
+++ deploy.sh
@@ -1,8 +1,9 @@
 #!/bin/sh
 # cd ~/home/admin/dehy/dehy
 git pull
-python3.7 -m venv venv && source venv/bin/activate
-pip install -r requirements.txt && echo 'hello world'
+python3.7 -m venv venv
+source venv/bin/activate
+pip install -r requirements.txt
 python manage.py collectstatic --noinput
 python manage.py makemigrations
 python manage.py migrate
