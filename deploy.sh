903836cd07ba7100a678a1b52c39ab81eedb0e49
Kenneth Tuten <ktuten@fb.com>
2022-02-11 13:12:45 -0600

    test again

--- deploy.sh
+++ deploy.sh
@@ -1,12 +1,12 @@
 #!/bin/sh
-
-# cd ~/home/admin/dehy
+cd ~/home/admin/dehy/dehy
 source venv/bin/activate
 git checkout staging
 git pull
 pip install -r requirements.txt
 python manage.py makemigrations
 python manage.py migrate
-python manage.py collectstatic
+echo 'hello world'
+python manage.py collectstatic --noinput
 sudo systemctl restart nginx
 sudo systemctl restart gunicorn
