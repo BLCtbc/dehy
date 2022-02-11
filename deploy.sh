d067debb09b1d6c2234499d9c276b07f01a420a8
Kenneth Tuten <ktuten@fb.com>
2022-02-11 13:52:30 -0600

    =

--- deploy.sh
+++ deploy.sh
@@ -1,9 +1,8 @@
 #!/bin/sh
-cd ~/home/admin/dehy/dehy
+# cd ~/home/admin/dehy/dehy
 git pull
-source venv/bin/activate
-pip install -r requirements.txt
-echo 'hello world'
+python3.7 -m venv venv && source venv/bin/activate
+pip install -r requirements.txt && echo 'hello world'
 python manage.py collectstatic --noinput
 python manage.py makemigrations
 python manage.py migrate
