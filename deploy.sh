99c74a5d5882905dc0676d487876d331b78586d7
Kenneth Tuten <ktuten@fb.com>
2022-02-11 14:40:02 -0600

    updated deploy script

--- deploy.sh
+++ deploy.sh
@@ -1,10 +1,6 @@
 #!/bin/sh
 
 git pull
-cd ~/home/admin/dehy/dehy && source venv/bin/activate
-pip install -r requirements.txt
-python manage.py collectstatic --noinput
-python manage.py makemigrations
-python manage.py migrate
+source /home/admin/dehy/dehy/venv/bin/activate && pip install -r requirements.txt && python manage.py collectstatic --noinput && python manage.py makemigrations && python manage.py migrate
 sudo systemctl daemon-reload
 sudo systemctl restart nginx && sudo systemctl restart gunicorn
