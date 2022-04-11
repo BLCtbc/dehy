121a6bf714e86e807eab3be90c2fe3a2cfc7108c
Kenneth Tuten <ktuten@fb.com>
2022-04-11 17:37:45 -0500

    added scroll trigger file for faq page

---
+++ dehy/static/js/faq.js
@@ -0,0 +1,15 @@
+$(document).ready(function() {
+	window.addEventListener("scroll", function() {
+	  var elementTarget = document.querySelector(".faq-page-inner");
+	  var href = '/faq/';
+
+	  if (window.scrollY > elementTarget.offsetHeight) {
+		  href = '/faq/#contact';
+	  }
+	  document.querySelector(`nav a.nav-link[href='${href}']`).classList.toggle('active', true);
+	  document.querySelectorAll(`nav a.nav-link.active:not([href='${href}'])`).forEach(elem=>{
+		  elem.classList.toggle('active', false);
+	  });
+	});
+});
+
