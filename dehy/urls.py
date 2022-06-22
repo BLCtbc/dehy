9f1f801337a6fd9b7e41be60f028b43c3e1e6f25
Kenneth Tuten <ktuten@fb.com>
2022-06-22 09:43:08 -0500

    implemented sitemap

--- dehy/urls.py
+++ dehy/urls.py
@@ -1,6 +1,11 @@
 from django.apps import apps
 from django.urls import include, path
 from django.contrib import admin
+from django.contrib.sitemaps.views import sitemap
+from django.contrib.sitemaps import GenericSitemap
+from dehy.appz.generic.sitemaps import StaticViewSitemap
+
+sitemaps = {'static': StaticViewSitemap}
 
 urlpatterns = [
 	path('admin/', admin.site.urls),
@@ -8,6 +13,7 @@ urlpatterns = [
 	path('recipes/', include('dehy.appz.recipes.urls'), name='recipes'),
 	path('wholesale/', include('dehy.appz.wholesale.urls'), name='wholesale'),
 	path('', include(apps.get_app_config('dehy').urls[0])),
+	path('sitemap.xml', sitemap, {'sitemaps': sitemaps}, name='django.contrib.sitemaps.views.sitemap'),
 ]
 
 from django.conf import settings
