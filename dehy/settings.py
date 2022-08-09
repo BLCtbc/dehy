0baee55446426435483e7d7667e3f66291389f67
Kenneth Tuten <ktuten@fb.com>
2022-08-09 13:18:02 -0500

    added language code to global context

--- dehy/settings.py
+++ dehy/settings.py
@@ -113,6 +113,7 @@ SITE_ID = env.int('SITE_ID')
 MIDDLEWARE = [
 	'django.middleware.security.SecurityMiddleware',
 	'django.contrib.sessions.middleware.SessionMiddleware',
+	'django.middleware.locale.LocaleMiddleware',
 	'django.middleware.common.CommonMiddleware',
 	'django.middleware.csrf.CsrfViewMiddleware',
 	'django.contrib.auth.middleware.AuthenticationMiddleware',
